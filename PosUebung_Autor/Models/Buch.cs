70c061cd0dbfb235e01f0f791e446d599d6ceb35
Friend2868 <90833416+Friend2868@users.noreply.github.com>
2024-03-08 08:30:59 +0100

    Initial commit

---
+++ PosUebung_Autor/Models/Buch.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace PosUebung_Autor.Models
+{
+    public class Buch
+    {
+        public int Id { get; set; }
+        [MaxLength(100, ErrorMessage = "Maximum length is 100 characters!"), MinLength(5, ErrorMessage = "Mininum length is 5 characters!")]
+        public string Title { get; set; }
+        public List<string> Genres { get; set; } 
+        public int Pages { get; set; }
+        public int Stars { get; set; }
+
+        //Fremdschlüssel
+        public Autor? Autor { get; set; }
+        public int? AutorId { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} {Autor?.Name} {Pages} {Stars}";
+        }
+    }
+}
